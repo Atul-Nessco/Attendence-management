1c834b8f1ae495b8059996ebff1a1f17ca12d533
nessco1234 <app1.nessco@gmail.com>
2025-01-11 16:23:55 +0530

    doing multi cors *

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const attendanceRoutes = require('./routes/attendanceRoutes');
 const reportRoutes = require('./routes/reportRoutes');
 
 const app = express();
-const PORT = process.env.PORT  || 9008;
+const PORT = process.env.PORT || 9008;
 
 if (!PORT) {
   console.error('PORT is not defined in the environment variables');
@@ -16,22 +16,29 @@ if (!PORT) {
 
 function startServer(port) {
   if (port >= 65536) {
-      console.error('No available ports');
-      process.exit(1);
+    console.error('No available ports');
+    process.exit(1);
   }
-  app.listen(port, () => {
+  app
+    .listen(port, () => {
       console.log(`Server is running on port ${port}`);
-  }).on('error', err => {
+    })
+    .on('error', (err) => {
       if (err.code === 'EADDRINUSE') {
+        console.error(`Port ${port} is already in use, trying the next one.`);
+        startServer(port + 1);
       } else {
-          console.error('Server error:', err);
+        console.error('Server error:', err);
       }
-  });
+    });
 }
 
-
-
-app.use(cors());
+// Allow all CORS requests
+app.use(cors({
+  origin: '*', // Allows all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}));
 
 app.use(express.json({ limit: '50mb' }));
 
@@ -41,6 +48,7 @@ app.use('/api', reportRoutes);
 app.use('/testing', (req, res) => {
   res.send('This is a test endpoint');
 });
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
